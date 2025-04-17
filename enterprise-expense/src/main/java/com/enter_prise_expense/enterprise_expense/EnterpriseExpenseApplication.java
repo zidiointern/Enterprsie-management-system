0c962cba46751dc9456620b056aea4b911b8a556
vicky <vbala4055gmail.com>
2025-04-17 15:26:07 +0530

    added basic project

---
+++ enterprise-expense/src/main/java/com/enter_prise_expense/enterprise_expense/EnterpriseExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.enter_prise_expense.enterprise_expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseExpenseApplication {
+    
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseExpenseApplication.class, args);
+	}
+
+}
---
+++ enterprise-expense/src/test/java/com/enter_prise_expense/enterprise_expense/EnterpriseExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enter_prise_expense.enterprise_expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
